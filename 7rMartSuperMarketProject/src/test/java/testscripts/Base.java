971685615595b3840af04501cc0108665fab7bda
Divya <divyajolly1234@gmail.com>
2024-05-17 21:50:18 +0530

    latest code

--- 7rMartSuperMarketProject/src/test/java/testscripts/Base.java
+++ 7rMartSuperMarketProject/src/test/java/testscripts/Base.java
@@ -1,6 +1,9 @@
 package testscripts;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,17 +14,30 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
+import utilities.Constants;
 import utilities.ScreenShotUtility;
 import utilities.WaitUtility;
 
 public class Base {
 	WebDriver driver;
 	public ScreenShotUtility scrshot;
+	public Properties prop;
+	FileInputStream fs;
 	
 	@BeforeMethod(alwaysRun=true)	
 	@Parameters("browser")
 	public void initializeBrowser(String browser) throws Exception
 	{
+		try 
+		{
+			prop = new Properties();
+			fs = new FileInputStream(Constants.CONFIGFILE);
+			prop.load(fs);
+		} 
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
 		if(browser.equalsIgnoreCase("chrome"))
 		{
 			driver = new ChromeDriver();
@@ -38,7 +54,7 @@ public class Base {
 		{
 			throw new Exception("browser is not correct");
 		}
-		driver.get("https://groceryapp.uniqassosiates.com/admin/login");
+		driver.get(prop.getProperty("url"));
 		WaitUtility waitutility = new WaitUtility(driver);
 		waitutility.implicitWait(driver);
 		driver.manage().window().maximize();		
